18c8f59cc5f715393b92c0e2a0f6eeb3888cf124
Mirza Abdul Rahman <106723952+mirzarahmansaghir@users.noreply.github.com>
2024-12-03 07:45:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail ="mirarahmansaghir66@gmail.com"
+var accountPassword ="12345"
+accountCity ="FSD"
+let accountstate;
+
+accountEmail = "ab@ab@.com"
+accountPassword ="212121212"
+accountCity ="ISL"
+// accountId = 2 // not allowed 
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+// code readabilty should be high 
+
+// number =>  2 tp power 53
+// bigint 
+//string =>""
+//boolean = true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+//object
+
+console.log(typeof "mirza")
